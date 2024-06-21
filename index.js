ea4988049cb8191d1f8fa666f9ad087044332e12
Justin Merz <jrmerz@gmail.com>
2024-06-21 09:57:53 -0700

    quick fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const model = require('./lib/model');
 const pg = require('./lib/pg');
 const csv = require('./lib/csv');
 const source = require('./lib/source');
+const config = require('./lib/config');
 
 module.exports = {
   model, pg, csv, source, config
